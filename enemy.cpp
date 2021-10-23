117637f230c19d3f0cfb312ef05d4d3dd80c10d7
Peter Arato <it.arato@gmail.com>
2021-10-23 09:17:25 -0400

    Fix enemy draw offset

--- enemy.cpp
+++ enemy.cpp
@@ -7,7 +7,10 @@ void Enemy::init() { walker->init(frame); }
 
 void Enemy::update(const Rectangle &player) { walker->update(frame, player); }
 
-void Enemy::draw(int draw_offset) const { DrawRectangleRec(frame, BLACK); }
+void Enemy::draw(int scroll_offset) const {
+  DrawRectangle(frame.x - scroll_offset, frame.y, frame.width, frame.height,
+                BLACK);
+}
 
 Enemy::Enemy(Rectangle frame, std::unique_ptr<IWalker> walker)
     : frame(frame), walker(std::move(walker)) {}
