e266f9b6c6196ac485ddf499d1d1bb03d4a32ad0
Peter Arato <it.arato@gmail.com>
2021-11-06 14:42:05 -0400

    Better circler particles.

--- src/particles.cpp
+++ src/particles.cpp
@@ -1,8 +1,8 @@
 #include "particles.h"
 
-#define PARTICLE_FADE_STEP 0.02f
-#define PARTICLE_CIRCLER_ROT_STEP 0.2f
-#define PARTICLE_CIRCLER_DIST_STEP 2.0f
+#define PARTICLE_FADE_STEP 0.04f
+#define PARTICLE_CIRCLER_ROT_STEP 0.1f
+#define PARTICLE_CIRCLER_DIST_STEP 3.0f
 
 // EXPLOSION //////////////////////////////////////////////////////////////////
 
@@ -38,26 +38,30 @@ Circler::Circler(Rectangle start_frame, size_t particle_count) : particle_count(
   for (size_t i = 0; i < particle_count; i++) {
     particle_pos.emplace_back(start_frame.x + start_frame.width / 2, start_frame.y + start_frame.height / 2);
     rot_offs.emplace_back(randf() * PI * 2.0f);
-    dist_offs.emplace_back(randf() * 6.0f + 20.0f);
+    dist_offs.emplace_back(randf() * 20.0f + 20.0f);
   }
 }
 
 void Circler::draw(IntVector2D scroll_offset) const {
+  float toggle_rot;
   for (size_t i = 0; i < particle_count; i++) {
+    toggle_rot = i % 2 == 0 ? rot : -rot;
     DrawRectangle(
-            particle_pos[i].x - scroll_offset.x + (sinf(rot + rot_offs[i]) * (dist_offs[i] + dist)),
-            particle_pos[i].y - scroll_offset.y + (cosf(rot + rot_offs[i]) * (dist_offs[i] + dist)),
+            particle_pos[i].x - scroll_offset.x + (sinf(toggle_rot + rot_offs[i]) * (dist_offs[i] + dist)),
+            particle_pos[i].y - scroll_offset.y + (cosf(toggle_rot + rot_offs[i]) * (dist_offs[i] + dist)),
             3,
             3,
-            RED);
+            Fade(RED, fade));
   }
 }
 
 void Circler::update() {
   rot += PARTICLE_CIRCLER_ROT_STEP;
-  dist += PARTICLE_CIRCLER_DIST_STEP;
+  dist += dist_step;
+  dist_step /= 1.1f;
+  fade *= 0.95f;
 }
 
 [[nodiscard]] bool Circler::is_completed() const {
-  return rot >= PI * 2.0f;
+  return rot >= PI / 2.0f;
 }
