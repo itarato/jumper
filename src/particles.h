43d68886ff40b652682a8c870e913c176be2d867
Peter Arato <it.arato@gmail.com>
2021-11-05 22:56:58 -0400

    Better explosion.

--- src/particles.h
+++ src/particles.h
@@ -18,7 +18,7 @@ struct Explosion {
   Explosion(Rectangle start_frame, size_t particle_count) : particle_count(particle_count) {
     for (size_t i = 0; i < particle_count; i++) {
       float angle = randf() * PI * 2.0f;
-      float v = randf() * 10.0f;
+      float v = randf() * 2.0f + 5.0f;
       particle_v.emplace_back(sinf(angle) * v, cosf(angle) * v);
       particle_pos.emplace_back(start_frame.x + start_frame.width / 2, start_frame.y + start_frame.height / 2);
     }
