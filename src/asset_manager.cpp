539a8673332e9f4d09af8485a15fe7cf77587f6c
Peter Arato <it.arato@gmail.com>
2021-12-13 14:24:34 -0500

    Tidy

--- src/asset_manager.cpp
+++ src/asset_manager.cpp
@@ -6,12 +6,10 @@
 #include <iostream>
 
 AssetManager::~AssetManager() {
-  for (auto& texture_pair : textures)
-    UnloadTexture(texture_pair.second);
+  for (auto& texture_pair : textures) UnloadTexture(texture_pair.second);
   textures.clear();
 
-  for (auto& font_pair : fonts)
-    UnloadFont(font_pair.second);
+  for (auto& font_pair : fonts) UnloadFont(font_pair.second);
   fonts.clear();
 }
 
@@ -25,8 +23,7 @@ std::vector<Texture2D*> AssetManager::texture_list(
 
     sprintf(file_name, file_name_format, i);
 
-    if (!textures.contains(file_name))
-      break;
+    if (!textures.contains(file_name)) break;
 
     out.emplace_back(&textures[file_name]);
   }
@@ -36,8 +33,7 @@ std::vector<Texture2D*> AssetManager::texture_list(
 
 void AssetManager::preload_textures(std::string folder) {
   for (auto const& file_path : std::filesystem::directory_iterator{folder}) {
-    if (file_path.path().extension() != ".png")
-      continue;
+    if (file_path.path().extension() != ".png") continue;
 
     textures.insert({file_path.path().filename(),
                      LoadTexture(file_path.path().string().c_str())});
